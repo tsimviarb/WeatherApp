d668711d70b6cce0dbda2f33b5ba7f125e796934
TimCanMakeAll <timofeiverbickii4@gmail.com>
2022-09-02 23:03:17 +0300

    10 days fragment cleared

--- app/src/main/java/com/tim/weatherapp/fragments/TenDaysFragmentNew.kt
+++ app/src/main/java/com/tim/weatherapp/fragments/TenDaysFragmentNew.kt
@@ -13,69 +13,24 @@ import com.tim.weatherapp.R
 import com.tim.weatherapp.adapters.WeatherAdapter
 import com.tim.weatherapp.adapters.WeatherModel
 import com.tim.weatherapp.databinding.FragmentTenDaysNewBinding
+import com.tim.weatherapp.databinding.FragmentTodayInfoBinding
 import com.tim.weatherapp.databinding.FragmentTomorrowBinding
 import org.json.JSONArray
 import org.json.JSONObject
 import kotlin.math.roundToInt
 
 class TenDaysFragmentNew : Fragment() {
-
-    private lateinit var binding: FragmentTenDaysNewBinding
+    private lateinit var binding: FragmentTodayInfoBinding
     private lateinit var adapter: WeatherAdapter
     private val model: MainViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-
-        binding = FragmentTenDaysNewBinding.inflate(inflater, container, false)
-        return binding.root
+        ): View? {
+        return null
     }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        initRecyclerView()
-        model.liveDataList.observe(viewLifecycleOwner){
-
-            adapter.submitList(getTenDaysModel(it))
-        }
-    }
-
-    private fun initRecyclerView() = with(binding){
-        adapter = WeatherAdapter()
-        RecyclerViewTenDaysNew.layoutManager = LinearLayoutManager(activity)
-        RecyclerViewTenDaysNew.adapter = adapter
-    }
-
-    private fun getTenDaysModel(weatherItem: WeatherModel): List<WeatherModel>{
-
-        val hoursArray = JSONArray(weatherItem)
-        val list = ArrayList<WeatherModel>()
-
-        for (i in 0 until hoursArray.length()) {
-
-            val windSpeedMS = ((hoursArray[i] as JSONObject).getString("wind_mph")
-                .toDouble() * 2.8).roundToInt()
-
-            val item = WeatherModel(
-                "",
-                (hoursArray[i] as JSONObject).getString("time"),
-                "",
-                (hoursArray[i] as JSONObject).getString("temp_c") + "°C",
-                "",
-                "",
-                "${windSpeedMS / 10}.${windSpeedMS % 10} m/s",
-                (hoursArray[i] as JSONObject)
-                    .getJSONObject("condition").getString("icon"),
-                ""
-            )
-            list.add(item)
-        }
-        return list
-    }
-
+    
     companion object {
 
         @JvmStatic
